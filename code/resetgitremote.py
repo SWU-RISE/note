aa343dbd3842d76068a40269c78f9c3ffa7f2b9d
liyun dai <dlyun2002@gmail.com>
2018-03-27 13:52:20 +0800

    oK

---
+++ code/resetgitremote.py
@@ -0,0 +1,6 @@
+#A remote name, for example, origin
+#A remote URL, for example, https://github.com/user/repo.git
+
+git remote add origin https://github.com/user/repo.git  #Set a new remote
+git remote -v # Verify new remote
+
